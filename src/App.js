620893eefe6988619f5f6a86cf69375aac0e0d6a
aminmlegy <aminmlegy@gmail.com>
2021-12-18 12:37:07 +0200

    first App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import photo2 from "./photo2.jpg";
+import "./Styl.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div
+        style={{
+          border: "solid 1px black",
+          maxWidth: "70%",
+          margin: "30px auto",
+          textAlign: "center",
+          padding: "30px",
+        }}>
+        <h1 className='title'>Your name here</h1>
+        <p className='red title'>
+          loremloremloremloremloremloremloremloremloremloremloremloremloremloremloremlorem
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <br />
+
+        <div className='flex'>
+          <img src='./photo1.jpg' alt='prev' />
+          <img src={photo2} alt='next' />
+        </div>
+        <br />
+        <video
+          width='50%'
+          height='240'
+          controls
+          style={{ margin: "15px auto" }}>
+          <source src='myVideo.mp4' type='video/mp4' />
+        </video>
+      </div>
+      <footer>
+        <section className='copyw'>
+          <p>copy right C</p>
+        </section>
+        <section className='links'>
+          <a href='#'>Facebook</a>
+          <a href='#'>google</a>
+          <a href='#'>linkein</a>
+        </section>
+      </footer>
+    </>
   );
 }
 
